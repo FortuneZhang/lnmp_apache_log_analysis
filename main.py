99115fb2ca38a8f9f65c1a9ed2d4ccf5adcedcab
fortunezhang <zhangfortune527@gmail.com>
2017-12-01 11:22:51 +0800

    fortune-sort ip request list

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ analysis_middle_result_file_name = str(time.time()) + '.txt'
 if __name__ == '__main__':
     format_file.format('www.miaoyoutong.com-access_log_2017_10_16_100000.log', analysis_middle_result_file_name)
     result = analysis_file.analysis(analysis_middle_result_file_name)
-    for ip,count in result.items():
-        print(ip + '->' + str(count))
+
+    result = sorted(result.items(), key=lambda d: d[1], reverse=True)
+    for item in result:
+        print(item[0] , str(item[1]),sep='->')
 
 
